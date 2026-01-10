592bd106289692e7028f45b8fcbb930614badf59
ppanagi <P.Panagi-1@student.tudelft.nl>
2026-01-10 19:31:50 +0100

    Incorporated reserved names and recursive functions in uniquify + tests

--- src/uniquify.rs
+++ src/uniquify.rs
@@ -4,7 +4,7 @@
 use std::collections::HashMap;
 
 use crate::lang::*;
-
+use crate::reserved::RESERVED_FUNCTION_NAMES;
 // ----------------------------------------------- Helper
 // ------------------------------------------------------
 
@@ -15,25 +15,38 @@ struct Context {
     /// Each scope is represented as a HashMap from original names to renamed
     /// names and are stored in a stack-like vector, where the last element
     /// is the current scope.
-    scopes: Vec<HashMap<String, String>>,
+    var_scopes: Vec<HashMap<String, String>>,
+
+    /// Function and variable names live in different namespaces in order to
+    /// allow function name shadowing without problems
+    fun_scopes: HashMap<String, String>,
 
     /// A global counter used for generating unique names across the program.
     counter: usize,
 }
 
 impl Context {
-    /// Create a new Context, initialize its counter to zero, and push on it an
-    /// empty HashMap. # Returns
+    /// Create a new Context, initialize its counter to zero, and push two empty
+    /// HashMaps as the variable and function scopes.
+    /// # Returns
     /// An initialized empty Context.
     fn new() -> Self {
+        let mut global = HashMap::new();
+
+        for &name in RESERVED_FUNCTION_NAMES.iter() {
+            global.insert(name.to_string(), name.to_string());
+        }
+
         Self {
-            scopes: vec![HashMap::new()],
+            var_scopes: vec![HashMap::new()],
+            fun_scopes: global,
             counter: 0,
         }
     }
 
     /// Generates a new unique name for a given identifier by appending to it
-    /// the current counter # Arguments
+    /// the current counter
+    /// # Arguments
     /// * 'name' - The identifier to be renamed
     /// # Returns
     /// The string containing the identifier's new name
@@ -46,44 +59,31 @@ impl Context {
     /// Pushes a new empty scope onto the scope stack when entering a new block
     /// or function.
     fn enter_scope(&mut self) {
-        self.scopes.push(HashMap::new());
+        self.var_scopes.push(HashMap::new());
     }
 
     /// Pops the top scope from the scope stack when exiting a block or
     /// function.
     fn exit_scope(&mut self) {
-        self.scopes.pop();
+        self.var_scopes.pop();
     }
 
-    /// Binds a given identifier to a newly generated unique name and stores it
-    /// in the current scope. # Arguments
+    /// Binds a given variable to a newly generated unique name and stores it
+    /// in the current variable scope.
+    /// # Arguments
     /// * 'name' - The original identifier to bind.
     /// # Returns
     /// The newly generated unique identifier as a string.
-    fn bind(&mut self, name: &str) -> String {
+    fn bind_var(&mut self, name: &str) -> String {
         let new_name = self.rename(name);
-        self.scopes
+        self.var_scopes
             .last_mut()
             .unwrap()
             .insert(name.to_string(), new_name.clone());
         new_name
     }
 
-    /// Binds an identifier without renaming it - currently only used for not
-    /// renaming the "main" function, as it is assumed that all programs
-    /// have only one main defined. # Arguments
-    /// * 'name' - The identifier to bind without renaming.
-    /// # Returns
-    /// The same identifier string.
-    fn bind_without_rename(&mut self, name: &str) -> String {
-        self.scopes
-            .last_mut()
-            .unwrap()
-            .insert(name.to_string(), name.to_string());
-        name.to_string()
-    }
-
-    /// Looks up the unique name associated with an identifier in the scope
+    /// Looks up the unique name associated with a variable in the scope
     /// stack from the innermost to the outermost scope.
     /// # Arguments
     /// * 'name' - The original identifier to look up.
@@ -92,13 +92,44 @@ impl Context {
     /// # Panics
     /// If the identifier is not bound in any active scope (e.g. using a
     /// variable defined in an inner block, outside of that block).
-    fn lookup(&self, name: &str) -> String {
-        for scope in self.scopes.iter().rev() {
+    pub fn lookup_var(&self, name: &str) -> String {
+        for scope in self.var_scopes.iter().rev() {
             if let Some(n) = scope.get(name) {
                 return n.clone();
             }
         }
-        panic!("Unbound identifier: {}", name);
+        panic!("Unbound variable: {}", name);
+    }
+
+    /// Binds a given function to a newly generated unique name and stores it
+    /// in the current function scope.
+    /// # Arguments
+    /// * 'name' - The original identifier to bind.
+    /// # Returns
+    /// The newly generated unique identifier as a string.
+    pub fn bind_fun(&mut self, name: &str) -> String {
+        let new_name = if name == "main" || RESERVED_FUNCTION_NAMES.contains(&name) {
+            name.to_string()
+        } else {
+            self.rename(name)
+        };
+        self.fun_scopes.insert(name.to_string(), new_name.clone());
+        new_name
+    }
+
+    /// Looks up the unique name associated with a function in the global scope
+    /// # Arguments
+    /// * 'name' - The original identifier to look up.
+    /// # Returns
+    /// The currently active unique name for that identifier.
+    /// # Panics
+    /// If the function is not defined in the global scope.
+    pub fn lookup_fun(&self, name: &str) -> String {
+        if let Some(n) = self.fun_scopes.get(name) {
+            n.clone()
+        } else {
+            panic!("Undefined function: {}", name);
+        }
     }
 }
 
@@ -108,12 +139,20 @@ impl Context {
 /// Offers the uniquify pass publicly via Program::uniquify
 impl Program {
     /// Produces a version of the program where all variable and function names
-    /// are unique. # Returns
+    /// are unique.
+    /// # Returns
     /// A new Program AST with all its names uniquified but with the same
     /// functionality.
     pub fn uniquify(&self) -> Self {
         let mut u = Context::new();
 
+        // First, bind all function names
+        // Helps with recursive / mutually recursive functions
+        for f in &self.0 {
+            u.bind_fun(&f.name);
+        }
+
+        // Then, enter those functions and uniquify them
         let functions = self
             .0
             .iter()
@@ -125,27 +164,19 @@ impl Program {
 }
 
 /// Renames a single function, its parameters, and body.
-/// However, it keeps the main function unrenamed.
 /// # Arguments
 /// * 'f' - The function to uniquify.
 /// * 'u' - The entire current Context.
 /// # Returns
 /// A new Function`AST with all identifiers uniquely renamed.
 fn uniquify_function(f: &Function, u: &mut Context) -> Function {
-    let name = if f.name == "main" {
-        // Retaining 'main' works well with the interpreter
-        u.bind_without_rename(&f.name)
-    } else {
-        u.bind(&f.name)
-    };
-
     u.enter_scope();
 
     let parameters = f
         .parameters
         .iter()
         .map(|p| {
-            let new_name = u.bind(&p.name);
+            let new_name = u.bind_var(&p.name);
             Parameter {
                 name: new_name,
                 ty: p.ty,
@@ -157,7 +188,7 @@ fn uniquify_function(f: &Function, u: &mut Context) -> Function {
     u.exit_scope();
 
     Function {
-        name,
+        name: u.lookup_fun(&f.name),
         parameters,
         ret_type: f.ret_type,
         body,
@@ -195,11 +226,11 @@ fn uniquify_expr(e: &Expr, u: &mut Context) -> Expr {
         },
 
         Expression::Call { fn_name, args } => Expression::Call {
-            fn_name: u.lookup(fn_name),
+            fn_name: u.lookup_fun(fn_name),
             args: args.iter().map(|a| uniquify_expr(a, u)).collect(),
         },
 
-        Expression::Var(name) => Expression::Var(u.lookup(name)),
+        Expression::Var(name) => Expression::Var(u.lookup_var(name)),
         Expression::Int(i) => Expression::Int(*i),
         Expression::Bool(b) => Expression::Bool(*b),
         Expression::Unit => Expression::Unit,
@@ -233,7 +264,7 @@ fn uniquify_stmt(stmt: &Statement, u: &mut Context) -> Statement {
     match stmt {
         Statement::Declaration { name, ty, val } => {
             let val = uniquify_expr(val, u);
-            let new_name = u.bind(name);
+            let new_name = u.bind_var(name);
             Statement::Declaration {
                 name: new_name,
                 ty: *ty,
@@ -242,7 +273,7 @@ fn uniquify_stmt(stmt: &Statement, u: &mut Context) -> Statement {
         }
 
         Statement::Assignment { name, val } => Statement::Assignment {
-            name: u.lookup(name),
+            name: u.lookup_var(name),
             val: uniquify_expr(val, u),
         },
 
--- tests/uniquify_test.rs
+++ tests/uniquify_test.rs
@@ -2,7 +2,7 @@
 use std::collections::HashSet;
 
 use untitled::lang::*;
-
+use untitled::reserved::RESERVED_FUNCTION_NAMES;
 // ----------------------------------------------- Helper
 // ------------------------------------------------------
 
@@ -14,14 +14,23 @@ use untitled::lang::*;
 /// 'Ok(())' if all names are unique; otherwise, 'Err(name)' for the first
 /// duplicate it finds.
 fn assert_unique(prog: &Program) -> Result<(), String> {
-    let mut seen: HashSet<String> = HashSet::new();
+    let mut seen_funs: HashSet<String> = RESERVED_FUNCTION_NAMES.iter().map(|s| s.to_string()).collect();
 
     for func in &prog.0 {
-        if !seen.insert(func.name.clone()) {
-            return Err(format!("Duplicate function name: {}", func.name));
+        if !RESERVED_FUNCTION_NAMES.contains(&func.name.as_str()) {
+            if !seen_funs.insert(func.name.clone()) {
+                return Err(format!("Duplicate function name: {}", func.name));
+            }
         }
 
-        check_expr(&func.body, &mut seen)?;
+        let mut local_seen_vars = HashSet::new();
+        for param in &func.parameters {
+            if !local_seen_vars.insert(param.name.clone()) {
+                return Err(format!("Duplicate parameter name in function {}: {}", func.name, param.name));
+            }
+        }
+
+        check_expr(&func.body, &mut local_seen_vars)?;
     }
 
     Ok(())
@@ -65,11 +74,12 @@ fn check_expr(expr: &Expr, seen: &mut HashSet<String>) -> Result<(), String> {
             statements,
             expr: inner_expr,
         } => {
+            let mut block_seen = seen.clone();
             for stmt in statements {
-                check_stmt(stmt, seen)?;
+                check_stmt(stmt, &mut block_seen)?;
             }
             if let Some(e) = inner_expr {
-                check_expr(e, seen)?;
+                check_expr(e, &mut block_seen)?;
             }
         }
         _ => {}
@@ -103,49 +113,14 @@ fn check_stmt(stmt: &Statement, seen: &mut HashSet<String>) -> Result<(), String
 /// # Arguments
 /// * 'original' - The original program AST.
 /// * 'uniquified' - The program AST after being passed through uniquify.
-fn assert_sound(original: &Program, uniquified: &Program) {
+fn assert_sound(original: &Program, uniquified: &Program) -> bool {
     let value1 = original.interpret().unwrap();
     let value2 = uniquified.interpret().unwrap();
-    assert_eq!(value1, value2);
+    value1 == value2
 }
 
 // ----------------------------------------------- Helper
 // ------------------------------------------------------
-
-/// Soundness test on a very simple program.
-#[test]
-fn soundness1() {
-    let src = r#"
-    def main(): Int := {
-        let x: Int = 5;
-        x
-    }
-    "#;
-    let original: Program = Program::parse(src).unwrap();
-    let uniquified = original.uniquify();
-
-    assert_sound(&original, &uniquified);
-}
-
-/// Soundness testing with multiple functions and shadowing.
-#[test]
-fn soundness2() {
-    let src = r#"
-    def fn(): Int := {
-        let x: Int = 2;
-        x
-    }
-
-    def main(): Int := {
-        let x: Int = 5;
-        x + fn()
-    }
-    "#;
-    let original: Program = Program::parse(src).unwrap();
-    let uniquified = original.uniquify();
-
-    assert_sound(&original, &uniquified);
-}
 
 /// Checks uniqueness in a very simple program.
 #[test]
@@ -158,6 +133,7 @@ fn correctness1() {
     let original: Program = Program::parse(src).unwrap();
     let uniquified = original.uniquify();
     assert!(assert_unique(&uniquified).is_ok());
+    assert!(assert_sound(&original, &uniquified));
 }
 
 /// Checks uniqueness in a very simple program.
@@ -172,6 +148,7 @@ fn correctness2() {
     let original: Program = Program::parse(src).unwrap();
     let uniquified = original.uniquify();
     assert!(assert_unique(&uniquified).is_ok());
+    assert!(assert_sound(&original, &uniquified));
 }
 
 /// Checks uniqueness in a program with a block.
@@ -192,6 +169,7 @@ fn correctness3() {
     let original: Program = Program::parse(src).unwrap();
     let uniquified = original.uniquify();
     assert!(assert_unique(&uniquified).is_ok());
+    assert!(assert_sound(&original, &uniquified));
 }
 
 /// Checks uniqueness in a program with multiple nested blocks.
@@ -213,6 +191,7 @@ fn correctness4() {
     let original: Program = Program::parse(src).unwrap();
     let uniquified = original.uniquify();
     assert!(assert_unique(&uniquified).is_ok());
+    assert!(assert_sound(&original, &uniquified));
 }
 
 /// Checks uniqueness in a program with an if statement.
@@ -221,7 +200,7 @@ fn correctness5() {
     let src = r#"
     def main(a: Int, b: Int): Int := {
         let a: Int = 4;
-        if a < 2 then {
+        if a < 10 then {
             a
         } else {
             2
@@ -230,6 +209,7 @@ fn correctness5() {
     let original: Program = Program::parse(src).unwrap();
     let uniquified = original.uniquify();
     assert!(assert_unique(&uniquified).is_ok());
+    assert!(assert_sound(&original, &uniquified));
 }
 
 /// Checks uniqueness in a program with a while loop.
@@ -237,17 +217,19 @@ fn correctness5() {
 fn correctness6() {
     let src = r#"
     def main(a: Int, b: Int): Bool := {
-        let a: Int = 1;
-        let d: Bool = (a == 1);
-        while d | false do {
-            let a: Int = d + 1;
-             d = !d;
+        let a: Int = 10;
+        let d: Bool = (a != 1);
+        while d do {
+            a = a - 1;
+            let a: Int = a;
+             d = (a != 1);
         };
         d
     }"#;
     let original: Program = Program::parse(src).unwrap();
     let uniquified = original.uniquify();
     assert!(assert_unique(&uniquified).is_ok());
+    assert!(assert_sound(&original, &uniquified));
 }
 
 /// Checks uniqueness in a program with nested if statements and while loops.
@@ -255,57 +237,201 @@ fn correctness6() {
 fn correctness7() {
     let src = r#"
     def main(a: Int, b: Int): Int := {
-        let c: Int = 2;
-        let d: Bool = !(c == 1);
-        let a: Int = -(1);
-        if a < 2 & true then {
-            while d | false do {
+        let b: Int = 10;
+        let d: Bool = true;
+        let a: Int = -1;
+        if a < 2 then {
+            while d do {
                 a = a + 1;
                 d = !d;
-                if a then {
-                    let d: Int = d + 1;
+                if a==0 then {
+                    let d: Int = b + 1;
+                    d
                 };
             };
             a
         } else {
-            d
+            a
         };
     }"#;
     let original: Program = Program::parse(src).unwrap();
     let uniquified = original.uniquify();
     assert!(assert_unique(&uniquified).is_ok());
+    assert!(assert_sound(&original, &uniquified));
 }
 
 /// Checks uniqueness in a complicated program.
 #[test]
 fn correctness8() {
     let src = r#"
-    def println(a: Int, b:  Int, c: Int): Unit := {
-        a = b + c;
-        while a | (b & c) do {
-            let x: Int = {
-                let a: Int = b;
-                b
-            };
+    def power(base: Int, exp: Int): Int := {
+        let result: Int = 1;
+        let i: Int = 0;
+
+        // multiply result by base exp times
+        while i < exp do {
+            result = result * base;
+            i = i + 1;
         };
+
+        result
     }
 
-    def main(a: Int, b: Int): Int := {
-        let c: Int = 2;
-        let d: Bool = !(c == 1);
-        let a: Int = -(1);
-        if a < 2 & true then {
-            while d | false do {
-                a = a + 1;
-                d = !d;
-                println(123, a, d);
-            };
-            a
-        } else {
-            d
+    def main(): Int := {
+        let b: Int = 3;
+        let e: Int = 4;
+
+        let p: Int = power(b, e);
+
+        let sum: Int = 0;
+        let j: Int = 1;
+        while j ≤ e do {
+            sum = sum + power(j, j);
+            j = j + 1;
         };
+
+       sum
+    }
+    "#;
+    let original: Program = Program::parse(src).unwrap();
+    let uniquified = original.uniquify();
+    assert!(assert_unique(&uniquified).is_ok());
+    assert!(assert_sound(&original, &uniquified));
+}
+
+/// Check uniqueness in recursive functions and var-fun shadowing
+#[test]
+fn correctness9() {
+    let src = r#"
+    def fib(x: Int): Int := if x ≤ 1 then x else fib(x - 1) + fib(x - 2)
+
+    def main(): Unit := {
+        let x: Int = 10;
+
+        let fib: Int = fib(x);
+        fib
     }"#;
     let original: Program = Program::parse(src).unwrap();
     let uniquified = original.uniquify();
     assert!(assert_unique(&uniquified).is_ok());
+    assert!(assert_sound(&original, &uniquified));
+}
+
+/// Check uniqueness in mutually recursive functions and var-fun shadowing
+#[test]
+fn correctness10() {
+    let src = r#"
+    def even(x: Int): Bool := if x == 0 then true else odd(x - 1)
+    def odd(x: Int): Bool := if x == 0 then false else even(x - 1)
+
+    def main(): Unit := {
+        let x: Int = 10;
+
+        even(x)
+    }"#;
+    let original: Program = Program::parse(src).unwrap();
+    let uniquified = original.uniquify();
+    assert!(assert_unique(&uniquified).is_ok());
+    assert!(assert_sound(&original, &uniquified));
+}
+
+/// Check uniqueness with different function ordering
+#[test]
+fn correctness11() {
+    let src = r#"
+    def even(x: Int): Bool := if x == 0 then true else odd(x - 1)
+
+    def main(): Unit := {
+        let x: Int = 10;
+
+        let y: Bool = even(x) | odd(x);
+    }
+
+    def odd(x: Int): Bool := if x == 0 then false else even(x - 1)"#;
+    let original: Program = Program::parse(src).unwrap();
+    let uniquified = original.uniquify();
+    assert!(assert_unique(&uniquified).is_ok());
+    assert!(assert_sound(&original, &uniquified));
+}
+
+/// Check uniqueness with reserved function names and shadowing
+#[test]
+fn correctness12() {
+    let src = r#"
+    def square(a: Int): Int := {
+        print(a);
+        a * a
+    }
+
+    def main(): Int := {
+        let a: Int = 10;
+        let println: Int = a + a;
+        let x: Int = {
+            println(println);
+            square(println)
+        };
+        x
+    }
+    "#;
+    let original: Program = Program::parse(src).unwrap();
+    let uniquified = original.uniquify();
+    assert!(assert_unique(&uniquified).is_ok());
+}
+
+/// Check uniqueness with shadowing parameters
+#[test]
+fn correctness13() {
+    let src = r#"
+    def main(a: Int, a: Int): Int := {
+       let a: Int = 1;
+       a
+    }
+    "#;
+    let original: Program = Program::parse(src).unwrap();
+    let uniquified = original.uniquify();
+    assert!(assert_unique(&uniquified).is_ok());
+    assert!(assert_sound(&original, &uniquified));
+}
+
+/// Check uniqueness with condition blocks
+#[test]
+fn correctness14() {
+    let src = r#"
+    def main(): Bool := {
+       let a: Int = 0;
+       if ({
+        let a: Int = 1;
+        if a < 0 then {
+            true
+        } else {
+            false
+        }
+       }) then true else false
+    }
+    "#;
+    let original: Program = Program::parse(src).unwrap();
+    let uniquified = original.uniquify();
+    assert!(assert_unique(&uniquified).is_ok());
+    assert!(assert_sound(&original, &uniquified));
+}
+
+/// Check uniqueness with unbound var access
+#[test]
+fn correctness15() {
+    let src = r#"
+    def main(): Int := {
+       let a: Int = 0;
+       if ({
+        let b: Int = 1;
+        if b < 0 then {
+            true
+        } else {
+            false
+        }
+       }) then b else a
+    }
+    "#;
+    let original: Program = Program::parse(src).unwrap();
+    let result = std::panic::catch_unwind(|| original.uniquify());
+    assert!(result.is_err());
 }
